0421f05a633fdf2d958173f6e54e1059726cf9f8
Noor-1994 <alshwaikinoor@gmail.com>
2026-07-28 10:50:43 -0400

    Complete lesson 4 assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 import './App.css';
+import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
-const todos = [
-  { id: 1, title: 'review resources' },
-  { id: 2, title: 'take notes' },
-  { id: 3, title: 'code out app' },
-];
-
 function App() {
-const [todoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todoTitle) {
+    const newTodo = {
+      id: Date.now(),
+      title: todoTitle,
+    };
+
+    setTodoList((previous) => [newTodo, ...previous]);
+  }
+
   return (
     <div>
+      <TodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </div>
   );
