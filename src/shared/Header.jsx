645c4f87479bc2533392b6faa3d50f7a2b618c53
Noor-1994 <alshwaikinoor@gmail.com>
2026-08-19 17:53:26 -0400

    Align Header with Lesson 7 requirements

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,18 +1,7 @@
-function Header({ token, onSetToken, onSetEmail }) {
-  const handleLogout = () => {
-    onSetToken('');
-    onSetEmail('');
-  };
-
+function Header() {
   return (
     <header>
       <h1>Todo List</h1>
-
-      {token && (
-        <button onClick={handleLogout}>
-          Log Out
-        </button>
-      )}
     </header>
   );
 }
